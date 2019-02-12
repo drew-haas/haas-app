881937ed9e0ba47ae513c9edd771c0e474b5efc0
Andrew Haas <anhaas@l-sff7sj1wv-m.gap.com>
2019-02-11 21:35:53 -0800

    update projects

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 
+const projects = [
+    {
+        title: 'Design Central',
+        classSuffix: 'design-central',
+        subTitle: 'An Interactive website for a product design and engineering company. Created with Drupal and custom Javascript animations and solutions.',
+        images: [],
+        type: 'work',
+        tags: ['animation', 'front-end', 'web'],
+        link: 'https://www.designcentral.com/'
+    },
+    {
+        title: 'The Laughing Cow',
+        classSuffix: 'laughing-cow',
+        subTitle: 'New and Old techniques used on this playful website for this well known dairy company.',
+        images: [],
+        type: 'work',
+        tags: ['animation', 'front-end', 'web'],
+        link: 'https://www.thelaughingcow.com/'
+    },
+    {
+        title: 'Moab',
+        classSuffix: 'moab',
+        subTitle: 'Exploring the Arches and desert walls of Moab, Utah.',
+        images: [],
+        type: 'life',
+        tags: ['photography', 'adventure', 'travels'],
+        link: 'https://www.instagram.com/p/BmLlaDSgp6d/'
+    },
+    {
+        title: 'Yosemite',
+        classSuffix: 'yosemite',
+        subTitle: 'A trip inside my favorite desktop wallpaper.',
+        images: [],
+        type: 'life',
+        tags: ['photography', 'adventure', 'travels'],
+        link: 'https://www.instagram.com/p/BmLlaDSgp6d/'
+    }
+];
+
+const gridItems = projects.map((proj, index) => 
+    <div className={'grid_row grid_row-' + proj.classSuffix + ' grid_type-' + proj.type} key={index}>
+        <div className="grid_item">{proj.title}</div>
+    </div>
+);
+
 class Hero extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    clearFilters() {
+        
+    }
+
     render() {
         return (
             <div className="heroSection">
                 <div className="bgtext">DrewHaas</div>
-                <div className="grid_container">
-                    <div className="grid_item grid_item-1">1</div>
-                    <div className="grid_item grid_item-2">2</div>
-                    <div className="grid_item grid_item-3">3</div>
-                    <div className="grid_item grid_item-4">4</div>
-                    <div className="grid_item grid_item-5">5</div>
-                    <div className="grid_item grid_item-6">6</div>
-                    <div className="grid_item grid_item-7">7</div>
-                    <div className="grid_item grid_item-8">8</div>
-                </div>
+                <div className="grid_container">{gridItems}</div>
+                <div className="rightAccentText" onClick={this.clearFilters}>Clear</div>
+                {/* <p>Welcome. My name is Drew Haas and I'm a Web Developer from Columbus, Ohio currently living in San Francisco.</p> */}
             </div>
         );
     }
